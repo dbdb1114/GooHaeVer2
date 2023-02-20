95747c7b54f2de98977e2e13d7b3713e275d1191
dbdb1114 <105846655+dbdb1114@users.noreply.github.com>
2023-02-20 15:42:43 +0900

    Delete goohae directory

--- goohae/src/ForProduct/ProductContainer/ProductContainer.js
+++
@@ -1,9 +0,0 @@
-import "./ProductContainer.css";
-
-export default function ProductContainer ({children}) {
-    return (
-        <div className="productContainer">
-            {children}
-        </div>
-    );
-}
--- goohae/src/ForReview/ReviewContainer/ReviewContainer.js
+++
@@ -1,9 +0,0 @@
-import "./ReviewContainer.css";
-
-export default function ReviewContainer ({children}) {
-    return (
-        <section className="ReviewContainer">
-            {children}
-        </section>
-    );
-}
